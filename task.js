3518a80f506363c2081e3a225bf31740a3ec641a
Anna Kuznetsova <akry67@gmail.com>
2022-09-09 22:49:28 +0400

    -discriminant

--- task.js
+++ task.js
@@ -5,9 +5,7 @@ function solveEquation(a, b, c) {
 
 let discriminant = b**2-4*a*c;
 
-if(discriminant < 0){
-  arr;
-}else if(discriminant == 0){
+if(discriminant == 0){
 arr[0] = -b/(2*a);
 } else if(discriminant > 0){
   arr[0] = (-b + Math.sqrt(discriminant))/(2*a);
