037b8913347ed9339d512796dd979b603aff7033
Anna Kuznetsova <akry67@gmail.com>
2022-09-09 19:28:29 +0400

    initial commit

---
+++ task.js
@@ -0,0 +1,26 @@
+"use strict";
+function solveEquation(a, b, c) {
+  let arr = [];
+  // код для задачи №1 писать здесь
+
+let discriminant = b**2-4*a*c;
+
+if(discriminant < 0){
+  arr;
+}else if(discriminant == 0){
+arr[0] = -b/(2*a);
+} else if(discriminant > 0){
+  arr[0] = (-b + Math.sqrt(discriminant))/(2*a);
+  arr[1] = (-b - Math.sqrt(discriminant))/(2*a);
+}
+
+  return arr; // array
+};
+
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   let totalAmount;
+
+//   // код для задачи №2 писать здесь
+
+//   return totalAmount;
+// }
